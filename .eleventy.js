4e9b3d86229bbde1aa4c9af4a66630e7a4d89fda
jamestae <jamestae7@gmail.com>
2023-04-06 10:53:07 -0400

    srcset added

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,31 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+    let metadata = await Image(src, {
+      widths: [300, 600, 900],
+      formats: ["jpeg", "webp"],
+      outputDir: "./_site/assets/images",
+      urlPath: "/assets/images/"
+    });
+  
+    let imageAttributes = {
+      alt,
+      sizes,
+      loading: "lazy",
+      decoding: "async",
+    };
+  
+    let srcset = metadata.jpeg.map((entry) => `${entry.url} ${entry.width}w`).join(", ");
+  
+    return `<img src="${metadata.jpeg[0].url}" srcset="${srcset}" ${Object.keys(imageAttributes).map(key => `${key}="${imageAttributes[key]}"`).join(" ")}>`;
+  }
+
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/admin');
+    eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
 
     return {
         dir: {
